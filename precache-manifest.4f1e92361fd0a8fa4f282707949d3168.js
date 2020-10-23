5354da950d3ab2effce8ad6f2577988d854d171f
Rasel Mahmud <raselm606@gmail.com>
2020-10-24 03:12:52 +0600

    Updates

---
+++ precache-manifest.4f1e92361fd0a8fa4f282707949d3168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be1246862fe5255dd69ba5c74de18eb",
+    "url": "/color-picker-React/index.html"
+  },
+  {
+    "revision": "cf94f8e85834466bdf3b",
+    "url": "/color-picker-React/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "591814cf3dc50d849a79",
+    "url": "/color-picker-React/static/css/main.58908f97.chunk.css"
+  },
+  {
+    "revision": "cf94f8e85834466bdf3b",
+    "url": "/color-picker-React/static/js/2.4a05af90.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/color-picker-React/static/js/2.4a05af90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591814cf3dc50d849a79",
+    "url": "/color-picker-React/static/js/main.3d285466.chunk.js"
+  },
+  {
+    "revision": "e65a66777a1feb6883ba",
+    "url": "/color-picker-React/static/js/runtime-main.ec1ba404.js"
+  }
+]);
